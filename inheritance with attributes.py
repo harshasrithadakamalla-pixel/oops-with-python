a5f53fd7142e754ce8e8b9fdc9931ce7c37b661b
harshasrithadakamalla-pixel <harshasrithadakamalla@gmail.com>
2026-09-07 16:05:23 +0530

    inheritance with attributes

---
+++ inheritance with attributes.py
@@ -0,0 +1,9 @@
+class college:
+    def study(self):
+        print("college is a place to study")
+class kprit(college):
+    def ghanpur(self):
+        print("kprit is in ghanpur")
+d1=kprit()
+d1.study()
+d1.ghanpur()
